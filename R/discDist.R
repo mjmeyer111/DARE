77d367f93e7ce72ff538d9120604554691c62b97
mjmeyer111 <mjm9ff@virginia.edu>
2023-06-13 11:10:24 -0400

    Remove discDist more thoroughly (but still temporarily)

--- R/discDist.R
+++ R/discDist.R
@@ -37,100 +37,99 @@
 #' geom_abline(slope = ddOut$estimates$slope, intercept = ddOut$estimates$intercept) +
 #' geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2)
 #' @importFrom vcd goodfit
-#' @importFrom magrittr pipe
 #' @import tibble
-discDist <- function (
-    x,
-    type = c("poisson", "binomial", "nbinomial"),
-    size = NULL,
-    lambda = NULL,
-    conf_int = TRUE,
-    conf_level = 0.95
-)
-{
-  if (is.vector(x)) {
-    x <- table(x)
-  }
-  if (is.table(x)) {
-    if (length(dim(x)) > 1)
-      stop("x must be a 1-way table")
-    freq <- as.vector(x)
-    count <- as.numeric(names(x))
-  }
-  else {
-    if (!(!is.null(ncol(x)) && ncol(x) == 2))
-      stop("x must be a 2-column matrix or data.frame")
-    freq <- as.vector(x[, 1])
-    count <- as.vector(x[, 2])
-  }
-  myindex <- (1:length(freq))[freq > 0]
-  mycount <- count[myindex]
-  myfreq <- freq[myindex]
-  switch(match.arg(type), poisson = {
-    par.ml <- suppressWarnings(goodfit(x, type = type)$par$lambda)
-    phi <- function(nk, k, N, size = NULL) ifelse(nk > 0,
-                                                  lgamma(k + 1) + log(nk/N), NA)
-    y <- phi(myfreq, mycount, sum(freq))
-    if (!is.null(lambda)) y <- y + lambda - mycount * log(lambda)
-    fm <- lm(y ~ mycount)
-    par.estim <- exp(coef(fm)[2])
-    names(par.estim) <- "lambda"
-    txt <- "exp(slope)"
-    if (!is.null(lambda)) {
-      par.estim <- par.estim * lambda
-      txt <- paste(txt, "x lambda")
-    }
-  }, binomial = {
-    if (is.null(size)) {
-      size <- max(count)
-      warning("size was not given, taken as maximum count")
-    }
-    par.ml <- suppressWarnings(goodfit(x,
-                                       type = type,
-                                       par = list(size = size))$par$prob)
-    phi <- function(nk, k, N, size = NULL)
-      log(nk) - log(N * choose(size, k))
-    y <- phi(myfreq, mycount, sum(freq), size = size)
-    fm <- lm(y ~ mycount)
-    par.estim <- exp(coef(fm)[2])
-    par.estim <- par.estim/(1 + par.estim)
-    names(par.estim) <- "prob"
-  }, nbinomial = {
-    if (is.null(size)) {
-      par.ml <- suppressWarnings(goodfit(x, type = type)$par)
-      size <- par.ml$size
-      par.ml <- par.ml$prob
-    } else {
-      xbar <- weighted.mean(mycount, myfreq)
-      par.ml <- size/(size + xbar)
-    }
-    phi <- function(nk, k, N, size = NULL)
-      log(nk) - log(N * choose(size + k - 1, k))
-    y <- phi(myfreq, mycount, sum(freq), size = size)
-    fm <- lm(y ~ mycount)
-    par.estim <- 1 - exp(coef(fm)[2])
-    names(par.estim) <- "prob"
-  })
-  yhat <- ifelse(myfreq > 1.5, myfreq - 0.67, 1/exp(1))
-  yhat <- phi(yhat, mycount, sum(freq), size = size)
-  if (!is.null(lambda))
-    yhat <- yhat + lambda - mycount * log(lambda)
-  phat <- myfreq/sum(myfreq)
-  ci.width <-
-    qnorm(1 - (1 - conf_level)/2) *
-    sqrt(1 - phat)/sqrt(myfreq - (0.25 * phat + 0.47) * sqrt(myfreq))
-  RVAL <- cbind(count, freq, NA, NA, NA, NA, NA)
-  RVAL[myindex, 3:7] <- cbind(y,
-                              yhat,
-                              ci.width,
-                              yhat - ci.width,
-                              yhat + ci.width)
-  forPlot <- cbind(phat, RVAL) %>%
-    as.data.frame(.)
-  colnames(forPlot) <- c("p.hat", "count", "freq", "metameter", "ci.center", "ci.width", "lower", "upper")
-  list(forPlot = forPlot,
-       par.estim = par.estim,
-       slope = coef(fm)[2] %>% as.numeric(),
-       intercept = coef(fm)[1] %>% as.numeric()) %>%
-    return()
-}
+# discDist <- function (
+#     x,
+#     type = c("poisson", "binomial", "nbinomial"),
+#     size = NULL,
+#     lambda = NULL,
+#     conf_int = TRUE,
+#     conf_level = 0.95
+# )
+# {
+#   if (is.vector(x)) {
+#     x <- table(x)
+#   }
+#   if (is.table(x)) {
+#     if (length(dim(x)) > 1)
+#       stop("x must be a 1-way table")
+#     freq <- as.vector(x)
+#     count <- as.numeric(names(x))
+#   }
+#   else {
+#     if (!(!is.null(ncol(x)) && ncol(x) == 2))
+#       stop("x must be a 2-column matrix or data.frame")
+#     freq <- as.vector(x[, 1])
+#     count <- as.vector(x[, 2])
+#   }
+#   myindex <- (1:length(freq))[freq > 0]
+#   mycount <- count[myindex]
+#   myfreq <- freq[myindex]
+#   switch(match.arg(type), poisson = {
+#     par.ml <- suppressWarnings(goodfit(x, type = type)$par$lambda)
+#     phi <- function(nk, k, N, size = NULL) ifelse(nk > 0,
+#                                                   lgamma(k + 1) + log(nk/N), NA)
+#     y <- phi(myfreq, mycount, sum(freq))
+#     if (!is.null(lambda)) y <- y + lambda - mycount * log(lambda)
+#     fm <- lm(y ~ mycount)
+#     par.estim <- exp(coef(fm)[2])
+#     names(par.estim) <- "lambda"
+#     txt <- "exp(slope)"
+#     if (!is.null(lambda)) {
+#       par.estim <- par.estim * lambda
+#       txt <- paste(txt, "x lambda")
+#     }
+#   }, binomial = {
+#     if (is.null(size)) {
+#       size <- max(count)
+#       warning("size was not given, taken as maximum count")
+#     }
+#     par.ml <- suppressWarnings(goodfit(x,
+#                                        type = type,
+#                                        par = list(size = size))$par$prob)
+#     phi <- function(nk, k, N, size = NULL)
+#       log(nk) - log(N * choose(size, k))
+#     y <- phi(myfreq, mycount, sum(freq), size = size)
+#     fm <- lm(y ~ mycount)
+#     par.estim <- exp(coef(fm)[2])
+#     par.estim <- par.estim/(1 + par.estim)
+#     names(par.estim) <- "prob"
+#   }, nbinomial = {
+#     if (is.null(size)) {
+#       par.ml <- suppressWarnings(goodfit(x, type = type)$par)
+#       size <- par.ml$size
+#       par.ml <- par.ml$prob
+#     } else {
+#       xbar <- weighted.mean(mycount, myfreq)
+#       par.ml <- size/(size + xbar)
+#     }
+#     phi <- function(nk, k, N, size = NULL)
+#       log(nk) - log(N * choose(size + k - 1, k))
+#     y <- phi(myfreq, mycount, sum(freq), size = size)
+#     fm <- lm(y ~ mycount)
+#     par.estim <- 1 - exp(coef(fm)[2])
+#     names(par.estim) <- "prob"
+#   })
+#   yhat <- ifelse(myfreq > 1.5, myfreq - 0.67, 1/exp(1))
+#   yhat <- phi(yhat, mycount, sum(freq), size = size)
+#   if (!is.null(lambda))
+#     yhat <- yhat + lambda - mycount * log(lambda)
+#   phat <- myfreq/sum(myfreq)
+#   ci.width <-
+#     qnorm(1 - (1 - conf_level)/2) *
+#     sqrt(1 - phat)/sqrt(myfreq - (0.25 * phat + 0.47) * sqrt(myfreq))
+#   RVAL <- cbind(count, freq, NA, NA, NA, NA, NA)
+#   RVAL[myindex, 3:7] <- cbind(y,
+#                               yhat,
+#                               ci.width,
+#                               yhat - ci.width,
+#                               yhat + ci.width)
+#   forPlot <- cbind(phat, RVAL) %>%
+#     as.data.frame(.)
+#   colnames(forPlot) <- c("p.hat", "count", "freq", "metameter", "ci.center", "ci.width", "lower", "upper")
+#   list(forPlot = forPlot,
+#        par.estim = par.estim,
+#        slope = coef(fm)[2] %>% as.numeric(),
+#        intercept = coef(fm)[1] %>% as.numeric()) %>%
+#     return()
+# }
